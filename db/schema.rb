36df9a45a069f2dbb8f0b3b2ed3c0f6b0147a0d4
PatrickZanowski <zanowski26@hotmail.com>
2016-03-03 22:23:05 -0500

    Aion/Cabal added to list

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160304022605) do
+ActiveRecord::Schema.define(version: 20160304022822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,11 @@ ActiveRecord::Schema.define(version: 20160304022605) do
     t.integer  "community_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "messageboards", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
